e2b3c024e8cb95dfa62ed1b6ec918d5250b25d1d
naobumbumbum <81776783+naobumbumbum@users.noreply.github.com>
2021-04-02 02:35:53 +0500

    Update pick_a_card.rb

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,4 +1,5 @@
+#Вводим массивы
 values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
 suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades']
-
+#Выводим на экран
 puts "#{values.sample} of #{suits.sample}"
